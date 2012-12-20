caf2dd2a26b04059bc8167371f3951ac95aa7f42
Igor Maneschy <ismaneschy@gmail.com>
2012-12-20 15:10:11 -0200

    atualizando minha versão

--- app/models/leitor.rb
+++ app/models/leitor.rb
@@ -45,7 +45,6 @@ class Leitor
  	# 	require 'pry'
 		# binding.pry
 
-<<<<<<< HEAD
 		lines.each do |line|
 				if lines[0] == line
 					layout.header["campos"].each do |campo|
@@ -65,27 +64,6 @@ class Leitor
 						dipj.fichas << ficha
 					end
 				end
-=======
-				dipj.tipo = line[0..2]
-				dipj.reservado0 = line[3..3]
-				dipj.mes = line[4..5]
-				dipj.periodo = line[6..7]
-				dipj.cnpj_contribuinte = line[8..21]
-				dipj.ano_calendario = line[22..22]
-				dipj.declaracao_retificadora = line[23..23]
-				dipj.cnpj_estabelecimento = line[24..29]
-				dipj.existe_movimento_periodo = line[30..30]
-				dipj.saldo_credor_periodo_anterior = line[31..44]
-				dipj.debito = line[45..58]
-				dipj.credito = line[59..72]
-				dipj.saldo_apurado = line[73..86]
-				dipj.credor_devedor = line[87..87]
-				dipj.reservado1 = line[88..97]
-				dipj.delimitador_registro = line[98..99]				
-				dipj.save
-				 #binding.pry
-			end
->>>>>>> sem alterações
 
 		end
 
