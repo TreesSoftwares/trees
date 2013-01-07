9023cf5155227ea140598d16c49c03377f8e3708
Paulo Moura <paulociecomp@gmail.com>
2013-01-07 03:16:55 -0300

    automatizacao da geracao de atributos no layout

--- app/models/leitor.rb
+++ app/models/leitor.rb
@@ -7,20 +7,19 @@ class Leitor
 
 		tables = page.css('table')
 		layout = Layout.new
-		#p tables[0].css('tr:eq(3) td:first').text
-		#
+
 		layout[:tipo_contribuicao] = tables[0].css('tr:first td:first').text
 		layout[:ficha] = tables[0].css('tr:eq(3) td:first').text
 		layout[:campos] = []
 
 		tables[1].css('tr').each do |link|
 			unless link == tables[1].css('tr')[0] || link.css('td').text.empty?
-				p '##'
 				puts link.css('td').text
 
 				layout[:campos] << {
 					ordem: link.css('td')[0].text,
 					nome: link.css('td')[1].text,
+					atributo: link.css('td')[1].text.gsub(/[0-9]|\.| |\(|\)|\-|\,/, ' ' => '_', '- ' => '').removeaccents.downcase.gsub(/^\_|\__/, '__' => '_'),
 					inicio: link.css('td')[2].text,
 					fim: link.css('td')[3].text,
 					tamanho: link.css('td')[4].text,
